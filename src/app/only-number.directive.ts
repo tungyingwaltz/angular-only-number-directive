82a3207177aea70effb7955bea47fae4396c2d9b
tungyingwaltz <tungyingwaltz@gmail.com>
2019-10-25 18:55:32 +0800

    1 file modified

--- src/app/only-number.directive.ts
+++ src/app/only-number.directive.ts
@@ -1,28 +1,33 @@
-import { Directive, HostListener, Input, ElementRef } from '@angular/core';
+import { Directive, HostListener, Input, ElementRef } from "@angular/core";
 
 @Directive({
-  selector: '[onlyNumber]'
+  selector: "[onlyNumber]"
 })
 export class OnlyNumberDirective {
-
-  regexStr = '^[0-9]*$';
+  regexStr = "^[0-9]*$";
   regEx = new RegExp(this.regexStr);
   @Input() onlyNumber: boolean;
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {}
 
-  @HostListener('change') onChange() {
-	if (this.onlyNumber) {
-		if (this.el.nativeElement.value) {
-			this.el.nativeElement.value = this.strEegEx(this.el.nativeElement.value);
-		  }
-	}
+  @HostListener("input", ["$event"]) oninput(event) {
+    let e = <InputEvent>event;
+    if (this.onlyNumber && e.inputType != "deleteContentBackward") {
+      if (this.regEx.test(this.el.nativeElement.value)) {
+        return;
+      } else {
+        console.log(e.detail)
+        this.el.nativeElement.value = this.strEegEx(
+          this.el.nativeElement.value
+        );
+      }
+    }
   }
 
-  @HostListener('keydown', ['$event']) onKeyDown(event) {
+  @HostListener("keydown", ["$event"]) onKeyDown(event) {
     let e = <KeyboardEvent>event;
     if (this.onlyNumber) {
-
-      if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
+      if (
+        [46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
         // Allow: Ctrl+A
         (e.keyCode === 65 && (e.ctrlKey || e.metaKey)) ||
         // Allow: Ctrl+C
@@ -32,38 +37,30 @@ export class OnlyNumberDirective {
         // Allow: Ctrl+X
         (e.keyCode === 88 && (e.ctrlKey || e.metaKey)) ||
         // Allow: home, end, left, right
-        (e.keyCode >= 35 && e.keyCode <= 39)) {
+        (e.keyCode >= 35 && e.keyCode <= 39)
+      ) {
         // let it happen, don't do anything
         return;
       }
       // Ensure that it is a number and stop the keypress
-      if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105) && e.keyCode != 229) {
+      if (
+        (e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) &&
+        (e.keyCode < 96 || e.keyCode > 105) &&
+        e.keyCode != 229
+      ) {
         e.preventDefault();
       }
     }
   }
 
-  @HostListener('keyup', ['$event']) onkeyup(event) {
-	let e = <KeyboardEvent>event;
-	if (this.onlyNumber){
-		if (this.regEx.test(this.el.nativeElement.value)) {
-			return;
-		  } else {
-			this.el.nativeElement.value = this.strEegEx(this.el.nativeElement.value);
-		  }
-	}
-  }
-  
   private strEegEx(s: string): string {
-    let newStr = s.split('').map(x => {
+    let newStr = s.split("").map(x => {
       if (this.regEx.test(x)) {
         return x;
-      }
-      else {
-        return '';
+      } else {
+        return "";
       }
     });
-    return newStr.join('');
+    return newStr.join("");
   }
-
 }
